d763eb7014550e488836c42e9f36f008c14e0cfc
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-12 14:32:36 +0530

    Adding Dispatch  Component and Service

---
+++ dispatch.sh
@@ -0,0 +1,22 @@
+dnf install golang -y
+
+useradd roboshop
+
+cp dispatch.service /etc/systemd/system/dispatch.service
+
+rm -rf /app
+mkdir /app
+
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
+cd /app
+unzip /tmp/dispatch.zip
+
+cd /app
+go mod init dispatch
+go get
+go build
+
+systemctl daemon-reload
+
+systemctl enable dispatch
+systemctl start dispatch
