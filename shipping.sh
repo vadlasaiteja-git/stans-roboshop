fb0e087e329e6830ad380acc3a50e5877d96ed0a
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-12 14:19:41 +0530

    Adding Shipping  Component and Service

---
+++ shipping.sh
@@ -0,0 +1,29 @@
+dnf install maven -y
+
+useradd roboshop
+
+cp shipping.service /etc/systemd/system/shipping.service
+
+
+rm -rf /app
+mkdir /app
+
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
+cd /app
+unzip /tmp/shipping.zip
+
+cd /app
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+
+dnf install mysql -y
+
+
+mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+systemctl daemon-reload
+systemctl enable shipping
+systemctl restart shipping
