2d835b6ddb3701289da01d8a8d4e8d478d3fbb52
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-13 23:17:41 +0530

    Adding Colours

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,22 @@
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+print_head(){
+  echo -e "\e[34m$*\e[0m"
+}
 
+print_head Disable Default Nginx
+dnf module disable nginx -y
+print_head Enable Nginx 24
+dnf module enable nginx:1.24 -y
+print_head Install Nginx
+dnf install nginx -y
+print_head Copy Nginx Config file
 cp nginx.conf /etc/nginx/nginx.conf
+print_head Clean Old Content
 rm -rf /usr/share/nginx/html/*
+print_head Download App content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+print_head Extract app content
+print_head unzip /tmp/frontend.zip
+Start Nginx Service
+print_head systemctl enable nginx
+print_head systemctl restart nginx
