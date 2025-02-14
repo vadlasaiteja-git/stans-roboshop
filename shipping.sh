234eafcdc99a776a9c585c4b0a7ac90814c43e3c
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-14 14:26:20 +0530

    Adding printhead

--- shipping.sh
+++ shipping.sh
@@ -1,8 +1,16 @@
 component=shipping
 source common.sh
+
 maven_app_setup
+print_head Install MySQL Client
 dnf install mysql -y
+
+print_head Load Schema
 mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/schema.sql
+
+print_head Load User creation
 mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/app-user.sql
+
+print_head Load Master Data
 mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/master-data.sql
 
