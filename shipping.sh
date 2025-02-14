9aca0bf5fff786fb9180135a756112a3fbb1e6e1
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-14 17:59:33 +0530

    Looping the Data load into MYSQL

--- shipping.sh
+++ shipping.sh
@@ -6,14 +6,16 @@ print_head Install MySQL Client
 dnf install mysql -y &>>$log_file
 exit_status_print $?
 
-print_head Load Schema
-mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
-exit_status_print $?
+for file in schema app-user master-data;do
+  print_head Load $file
+  mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/$file.sql &>>$log_file
+  exit_status_print $?
+done
 
-print_head Load User creation
-mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
-exit_status_print $?
+#print_head Load User creation
+#mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
+#exit_status_print $?
 
-print_head Load Master Data
-mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
-exit_status_print $?
+#print_head Load Master Data
+#mysql -h mysql-dev.saitejasroboshop.store -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
+#exit_status_print $?
